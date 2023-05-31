163603a65ef9cb6fd10117a7f4ce83017f881bf3
Uri Shteinberg <uri.shteinberg@gmail.com>
2023-05-31 15:58:48 +0300

    Add new root methods to Function

--- src/Function.java
+++ src/Function.java
@@ -4,4 +4,31 @@ public abstract class Function {
     public abstract String toString();
     public abstract Function derivative();
 
+    public double bisectionMethod(double a, double b, double epsilon){
+        double left = a, right = b;
+        while (right-left>epsilon){
+            double mid = (right+left) / 2;
+            if(valueAt(left)*valueAt(mid) > 0){
+                left = mid;
+            }
+            else {
+                right = mid;
+            }
+        }
+        return (left+right) / 2;
+    }
+
+    public double newtonRaphsonMethod(double a, double epsilon){
+        double xk = a;
+        double abs_xk = xk>0? xk : -xk;
+        while (abs_xk > epsilon){
+            xk = xk - (valueAt(xk) / derivative().valueAt(xk));
+            abs_xk = xk>0? xk : -xk;
+        }
+        return xk;
+    }
+
+//    public double taylorPolynomial(int n){
+//
+//    }
 }
