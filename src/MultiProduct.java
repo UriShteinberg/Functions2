a0b4a43fcc6c8d9bd22d7a2103a2cb030fbdd225
Uri Shteinberg <uri.shteinberg@gmail.com>
2023-06-01 13:55:50 +0300

    Fix Multi's

--- src/MultiProduct.java
+++ src/MultiProduct.java
@@ -1,13 +1,14 @@
 public class MultiProduct extends Function{
+    private Function f1;
+    private Function f2;
     private Function[] funcs;
     private int len;
 
-    public MultiProduct(Function... funcs){
+    public MultiProduct(Function f1, Function f2, Function... funcs){
+        this.f1 = f1;
+        this.f2 = f2;
         this.funcs = funcs.clone();
         this.len = funcs.length;
-        if(len<2){
-            System.out.println("exception: return to this!");
-        }
         for (int i=0; i < len; ++i){
             this.funcs[i] = funcs[i];
         }
@@ -15,6 +16,8 @@ public class MultiProduct extends Function{
     @Override
     public double valueAt(double x){
         double value = 1;
+        value *= f1.valueAt(x);
+        value *= f2.valueAt(x);
         for(Function f: funcs){
             value *= f.valueAt(x);
         }
@@ -24,6 +27,10 @@ public class MultiProduct extends Function{
     @Override
     public String toString() {
         String res = "(";
+        res += f1.toString();
+        res += " * ";
+        res += f2.toString();
+        if(len>0) res += " * ";
         for (int i = 0; i < len; i++) {
             res += funcs[i].toString();
             if (i< funcs.length-1) {
@@ -36,12 +43,26 @@ public class MultiProduct extends Function{
 
     @Override
     public Function derivative(){
+        Function der1 = new MultiProduct(f1.derivative(), f2, funcs);
+        Function der2 = new MultiProduct(f1, f2.derivative(), funcs);
         Function derivs[] = new Function[funcs.length];
         for (int i = 0; i < len; i++){
-            Function multis[] = funcs.clone();
+            Function multis[] = funcs;
             multis[i] = funcs[i].derivative();
-            derivs[i] = new MultiProduct(multis);
+            derivs[i] = new MultiProduct(f1, f2, multis);
         }
-        return new MultiSum(derivs);
+        return new MultiSum(der1, der2, derivs);
     }
 }
+
+
+//    @Override
+//    public Function derivative(){
+//        Function derivs[] = new Function[funcs.length];
+//        for (int i = 0; i < len; i++){
+//            Function multis[] = funcs.clone();
+//            multis[i] = funcs[i].derivative();
+//            derivs[i] = new MultiProduct(multis);
+//        }
+//        return new MultiSum(derivs);
+//    }
--- src/MultiSum.java
+++ src/MultiSum.java
@@ -1,13 +1,15 @@
 public class MultiSum extends Function{
+    private Function f1;
+    private Function f2;
     private Function[] funcs;
     private int len;
 
-    public MultiSum(Function... funcs){
+    public MultiSum(Function f1, Function f2, Function... funcs){
         this.funcs = funcs.clone();
         this.len = funcs.length;
-        if(len<2){
-            System.out.println("exception: return to this!");
-        }
+
+        this.f1 = f1;
+        this.f2 = f2;
         for (int i=0; i < len; ++i){
             this.funcs[i] = funcs[i];
         }
@@ -15,6 +17,8 @@ public class MultiSum extends Function{
     @Override
     public double valueAt(double x){
         double value = 0;
+        value += f1.valueAt(x);
+        value += f2.valueAt(x);
         for(Function f: funcs){
             value += f.valueAt(x);
         }
@@ -24,6 +28,10 @@ public class MultiSum extends Function{
     @Override
     public String toString() {
         String res = "(";
+        res += f1.toString();
+        res += " + ";
+        res += f2.toString();
+        if(len>0) res += " + ";
         for (int i = 0; i < len; i++) {
             res += funcs[i].toString();
             if (i< funcs.length-1) {
@@ -36,11 +44,13 @@ public class MultiSum extends Function{
 
     @Override
     public Function derivative(){
+        Function der1 = f1.derivative();
+        Function der2 = f2.derivative();
         Function derivs[] = new Function[funcs.length];
         for (int i = 0; i < len; i++){
             derivs[i] = funcs[i].derivative();
         }
 
-        return new MultiSum(derivs);
+        return new MultiSum(der1, der2, derivs);
     }
 }
