0f067f0e6317b163274678814e667d0955d1dc78
UriShteinberg <ushteinberg@campus.technion.ac.il>
2023-06-03 21:48:34 +0300

    Fixing NewtonRaphson

--- src/MultiProduct.java
+++ src/MultiProduct.java
@@ -59,7 +59,53 @@ public class MultiProduct extends Function{
 //        return new MultiSum(der1, der2);
 //    }
 //
-
+public Function[] funcsToFunc(){
+    Function[] all_together = new Function[len+2];
+    all_together[0] = f1;
+    all_together[1] = f2;
+    for(int i=2; i<len+2; ++i){
+        all_together[i] = funcs[i-2];
+    }
+    return all_together;
+}
+//    @Override
+//public Function derivative() {
+//    Function der1 = f1;
+//    Function der2 = f2;
+//    Function all_together[] = this.funcsToFunc();
+//    Function[] derivs = new Function[len+2];
+//    for (int i = 0; i < len; i++) {
+//        Function[] multis = all_together; // find alternative to the clone!!
+//        multis[0] = all_together[i].derivative();
+//        for (int j = 1; j < len+2; ++j) {
+//            if (j == i) continue;
+//            else if (j < i) {
+//                multis[j] = all_together[j - 1];
+//            } else {
+//                multis[j] = all_together[j];
+//            }
+//        }
+//        Function cut_multis[]= new Function[len];
+//        for (int j=0; j<len; ++j){
+//            cut_multis[j] = multis[j+2];
+//        }
+//        if(i==0){
+//            der1 = new MultiProduct(multis[0], multis[1], cut_multis);
+//        } else if (i==1) {
+//            der2 = new MultiProduct(multis[0], multis[1], cut_multis);
+//        }
+//        else {
+//            derivs[i] = new MultiProduct(multis[0], multis[1], cut_multis);
+//        }
+//    }
+//    if(len==0){
+//        return new MultiSum(der1, der2);
+//    }
+//    else {
+//        Function newArr[] = new Function[derivs.length+2];
+//        return new MultiSum(der1, der2, derivs);
+//    }
+//}
 
 
     @Override
